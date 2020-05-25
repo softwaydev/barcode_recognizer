2e7fdb466d1645235f8f726d58c819bf7fc0a86c
dyus <dyuuus@gmail.com>
2020-05-25 14:05:10 +0300

    add rotation for better matching

--- project/views.py
+++ project/views.py
@@ -1,7 +1,9 @@
+import cv2
+import imutils
+from pyzbar import pyzbar
+
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
-import cv2
-from pyzbar import pyzbar
 
 
 @api_view(['post'])
@@ -15,5 +17,10 @@ def upload(request):
     file_obj = request.FILES['filename']
     image = cv2.imread(file_obj.temporary_file_path())
     barcodes = pyzbar.decode(image)
-    print(barcodes)
+    if not barcodes:
+        for angle in [30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330]:
+            rotated = imutils.rotate_bound(image, angle)
+            barcodes = pyzbar.decode(rotated)
+            if barcodes:
+                break
     return JsonResponse({'text': barcodes[0].data.decode() if barcodes else ''})
